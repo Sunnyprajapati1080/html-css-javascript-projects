e3bf8a42db5f7acb893ddb53fecd1e1081f49ffe
sunny <saurabh1080i@gmail.com>
2022-03-11 08:22:05 -0800

    improved simple video player

--- 5.simple video player/script.js
+++ 5.simple video player/script.js
@@ -4,6 +4,9 @@ const open_btn = document.querySelector("button");
 const alert_box = document.querySelector(".alert");
 const history = document.querySelector("#history");
 const clear_hist = document.querySelector(".clear");
+const backward = document.querySelectorAll(".actions button")[1];
+const forward = document.querySelectorAll(".actions button")[2];
+const speed = document.querySelector("select");
 
 let history_items = []
 if(localStorage.getItem("history")!=null){
@@ -18,6 +21,7 @@ clear_hist.addEventListener("click",()=>{
     history.innerHTML = "<option value='history'>history</option>"
 })
 open_btn.addEventListener("click", () => {
+    if(url.value!=""){
     let file_url = "file://"+String(url.value)
     file_url = file_url.replace(/\s/g,"%20")
     if(file_url.includes(".mp4")==true){
@@ -26,34 +30,32 @@ open_btn.addEventListener("click", () => {
 	if(confirm("is this a webm file?")){
 	file_url = file_url+".webm"
         video.setAttribute("src",file_url)
-}else{
+    }else{
         file_url = file_url+".mp4"
         video.setAttribute("src",file_url)}
-
     }
     const fileName = video.src.replaceAll("%20"," ").split("/").reverse()[0].replace(".mp4","").slice(0,40)
     history_items.push({url:file_url,name:fileName})
-    history_items.length>5 ? history_items.shift():false
     history.innerHTML = ""
     history_items.forEach((item)=>{
     history.innerHTML += `<option value=${item.url}>${item.name}</option>`
 })
+    video.playbackRate = speed.value
     localStorage.setItem("history",JSON.stringify(history_items))
+}
 })
-video.addEventListener("error",(e)=>{
+video.addEventListener("error",()=>{
     alert_box.style.display = "flex"
     setTimeout(()=>{
     alert_box.style.display = "none"    
     },2000)
 })
 history.addEventListener("change",()=>{
-    history.value=="history" ? false: video.src = history.value
+    history.value=="history" ? false: video.src = history.value; video.playbackRate = speed.value
 }) 
-document.querySelector("select").addEventListener("change", () => {
-    video.playbackRate = document.querySelector("select").value
+speed.addEventListener("change", () => {
+    video.playbackRate = speed.value
 })
-const backward = document.querySelectorAll(".actions button")[1];
-const forward = document.querySelectorAll(".actions button")[2];
 backward.addEventListener("click", () => {
     video.currentTime -= 5
 })
